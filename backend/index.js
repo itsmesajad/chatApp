418379d350882252eb9fb8b8fd41efdb22f22fd4
SAJAD <itsmesajadkp@gmail.com>
2024-08-09 12:42:38 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000
+
+const app = express();
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
+
+
+
+// app.use(`/api/auth`, authRouter)
